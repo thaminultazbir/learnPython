4f73eaafb62cceb2ba4699a20292d823d9106c10
thaminultazbir <thaminultazbir1999@gmail.com>
2023-10-15 14:20:45 +0600

    display word less than 4

---
+++ FILE_Handle/displayLessThan 4char/engine.py
@@ -0,0 +1,12 @@
+def LessThan():
+    file = open("D:\\Asif\\python\\FILE_Handle\\displayLessThan 4char\\note.txt")
+    data = file.read()
+    words = data.split()
+
+    for word in words:
+        if (len(word) < 4):
+            print(word)
+    
+
+
+LessThan()
